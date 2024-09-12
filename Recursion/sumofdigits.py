63ab441e310b225d97b1a545dffb3e292bacca96
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-09-12 22:37:15 +0100

    Added: sum of digits

--- Recursion/sumofdigits.py
+++ Recursion/sumofdigits.py
@@ -6,6 +6,15 @@ def sumofdigits(num):
     if num == 0:
         return 0
     else:
-        return int(num%10) + sumofdigits(int(num/10))
+        return int(num % 10) + sumofdigits(int(num / 10))
 
 print(sumofdigits(123451))
+
+# def sumofdigits(num):
+#     assert num >= 0 and isinstance(num, int), "The number has to be a positive integer only!"
+#     if num == 0:
+#         return 0
+#     else:
+#         return num % 10 + sumofdigits(num // 10)
+
+# print(sumofdigits(123451))

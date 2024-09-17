4ec655fac30650d809a5d99aa18fd41d975b36b4
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-09-17 20:43:50 +0100

    Added: search element in array

--- Array/array.py
+++ Array/array.py
@@ -2,10 +2,18 @@ from array import *
 
 arr1 = array('i', [1, 2, 3, 5, 3, 5])
 
-def accessElement(array, index):
-    if index >= len(array):
-        print("Index not in the array")
-    else:
-        print(array(arr1[index]))
+# def accessElement(array, index):
+#     if index >= len(array):
+#         print("Index not in the array")
+#     else:
+#         print(array[index])
 
-accessElement(5)
+# accessElement(arr1, 5)
+
+def searchArray(array, value):
+    for i in array:
+        if i == value:
+            return array.index(value)
+    return "The element does not exist in the array"
+
+print(searchArray(arr1, 5))

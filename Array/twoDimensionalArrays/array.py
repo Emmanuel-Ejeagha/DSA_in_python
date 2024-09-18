45d678b42420e5c358beb19c77b60d34ce28681b
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-09-18 15:42:03 +0100

    Added: 2DArray

---
+++ Array/twoDimensionalArrays/array.py
@@ -0,0 +1,9 @@
+# Two dimensional arrays
+import numpy as np
+
+twoDArray = np.array([[2, 5, 65, 54],
+                     [34, 2, 90, 40],
+                     [30, 50, 24, 86],
+                     [30, 16, 122, 9]])
+
+print(type(twoDArray), twoDArray)

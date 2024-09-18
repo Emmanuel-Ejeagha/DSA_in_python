382a0e59b6f2052467ebf16e55bd274d42d8b36b
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-09-18 11:12:07 +0100

    Added: tranverse function

---
+++ Array/arrayPractice.py
@@ -0,0 +1,23 @@
+from array import *
+
+
+# 1. Create an array and tranverse
+my_arr = array('i', [1,2,3,4,5])
+
+for i in my_arr:
+    print(i)
+
+# 2. Access individual elements through indexes
+# 3. Append any value to the array using append() method
+# 4. Insert value in an array using insert() method
+# 5. Extend python array using extend() method
+# 6. Add items from list into array using formlist() method
+# 7. Remove any array element using remove() method
+# 8. Remove last array element using pop() method
+# 9. Fetch my element through its index using index() method
+# 10. Reverse a python array using reverse() method
+# 11. Get array buffer information through buffer_info() method
+# 12. Check for number of occurances of an element using count() method
+# 13. Convert array to string using tostring() method
+# 14. Convert array to a python list with same elements using tolist() method
+# 15. Append a string to char array using fromstring() method

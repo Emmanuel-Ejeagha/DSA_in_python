6a833a81bdd877a785a80c0ba79a2015ed1c843b
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-09-19 22:37:22 +0100

    modified

--- Array/twoDimensionalArrays/array.py
+++ Array/twoDimensionalArrays/array.py
@@ -2,8 +2,16 @@
 import numpy as np
 
 twoDArray = np.array([[2, 5, 65, 54],
-                     [34, 2, 90, 40],
-                     [30, 50, 24, 86],
+                     [34, 2, 90, 4],
+                     [30, 950, 24, 86],
                      [30, 16, 122, 9]])
 
 print(type(twoDArray), twoDArray)
+
+# Inserting a new column into the array
+new2DArray = np.insert(twoDArray, 0, [[200, 9, 3, 8]], axis=1)
+print(new2DArray)
+
+# Inserting a new row into the array
+new2DArray = np.insert(twoDArray, 0, [[11, 200, 9, 3]], axis=0)
+print(new2DArray)

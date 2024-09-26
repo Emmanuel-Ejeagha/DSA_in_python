d9fca2653f819d4776ab102389bd31bc69df587a
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-09-26 15:21:22 +0100

    Added: Linear search

---
+++ Array/twoDimensionalArrays/searching.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+twoDArray = np.array([[2, 5, 65, 54],
+                     [34, 2, 90, 4],
+                     [30, 950, 24, 86],
+                     [30, 16, 22, 9]])
+
+print(twoDArray)
+
+def search2DArray(array, value):
+    for x in range(len(array)):
+        for y in range(len(array[0])):
+            if array[x][y] == value:
+                return 'The value is located at index' + str(x)+" "+ str(y)
+    return 'The element is not found'
+
+print(search2DArray(twoDArray, 22))

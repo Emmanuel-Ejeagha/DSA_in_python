9d546a53e0d856a50857dcab1c002ad0b476a813
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-09-29 22:54:08 +0100

    Quiz and solution

--- Lists/listOperation.py
+++ Lists/listOperation.py
@@ -19,7 +19,32 @@ empty = []
 for i in empty:
     print("I am empty")
 
-# List operations / functions
+# * operator
 a = [123, '*']
 a *= 6
 print(a)
+
+# Quiz
+# total = 0
+# count = 0
+# while(True):
+#     prompt = input('Enter a number or (done to end): ')
+#     if prompt == 'done': break
+#     value = float(prompt)
+#     total += value
+#     count += 1
+# average = total / count
+    
+# print(f'sum: {total} Average: {average}')
+
+# Solution
+mylist = [] #  I can also used: mylist = list()
+while(True):
+    prompt = input('Enter a number or (done t o end): ')
+    if prompt == 'done': break
+    value = float(prompt)
+    mylist.append(value)
+    mySum = sum(mylist)
+    average = mySum / len(mylist)
+    
+print(f'sum: {mySum} Average: {average}')

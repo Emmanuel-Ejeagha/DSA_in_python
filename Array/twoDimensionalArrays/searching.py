a3b044154edfcd8e594239b0c691ce012ed76f0c
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-09-30 06:27:12 +0100

    updatedA

--- Array/twoDimensionalArrays/searching.py
+++ Array/twoDimensionalArrays/searching.py
@@ -3,7 +3,7 @@ import numpy as np
 twoDArray = np.array([[2, 5, 65, 54],
                      [34, 2, 90, 4],
                      [30, 950, 24, 86],
-                     [30, 6, 22, 9]])
+                     [30, 6, 4, 9]])
 
 print(twoDArray)
 
@@ -14,4 +14,4 @@ def search2DArray(array, value):
                 return 'The value is located at index '+ str(x)+" "+ str(y)
     return 'The element is not found'
 
-print(search2DArray(twoDArray, 6))
+print(search2DArray(twoDArray, 2))

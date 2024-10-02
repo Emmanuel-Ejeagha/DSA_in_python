64538510cf314eaa5f84b27ff08427b8fba6f9b8
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-10-02 19:05:51 +0100

    Added: any and all method

--- Dictionary/operatorsDict.py
+++ Dictionary/operatorsDict.py
@@ -6,3 +6,24 @@ print('name' in myDict)
 
 for key in myDict:
     print(key, myDict[key])
+
+
+#  all()
+newDict = {0: True, 1: False, 2: False}
+print(all(newDict))
+newDict = {1: True, 2: True}
+print(all(newDict))
+newDict = {}
+print(all(newDict))
+newDict = {0: False, 1: False, 2: False}
+print(all(newDict))
+
+# any()
+newDict = {0: True, 1: False, 2: False}
+print(any(newDict))
+newDict = {1: True, 2: True}
+print(any(newDict))
+newDict = {}
+print(any(newDict))
+newDict = {0: False, 1: False, 2: False}
+print(any(newDict))

62939f503b13d61324b1860c578eb0092fcc90d7
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-10-02 18:01:39 +0100

    deleteDict.py

--- Dictionary/deleteDict.py
+++ Dictionary/deleteDict.py
@@ -3,5 +3,8 @@
 myDict = {'name': 'Emmanuel', 'age': 20, 'state': 'Lagos', 'ethnicity': 'Igbo'}
 # print(myDict.pop('state'))
 # myDict.pop('state')
-print(myDict.popitem())
-print(myDict)
+# print(myDict.popitem())
+# print(myDict)
+
+del myDict
+

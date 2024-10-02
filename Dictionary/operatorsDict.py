2afeeed8571cc15df39d00db3873cf5c208c49b2
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-10-02 18:49:54 +0100

    Added: operators

---
+++ Dictionary/operatorsDict.py
@@ -0,0 +1,8 @@
+# Operators and Built in functions
+
+myDict = {'name': 'Emmanuel', 'age': 20, 'state': 'Lagos', 'ethnicity': 'Igbo'}
+
+print('name' in myDict)
+
+for key in myDict:
+    print(key, myDict[key])

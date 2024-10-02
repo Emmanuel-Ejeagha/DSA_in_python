7274793602c5c17b7748dc48a98c1539a3f74911
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-10-02 17:19:47 +0100

    Added: delete elements of dict

---
+++ Dictionary/deleteDict.py
@@ -0,0 +1,7 @@
+#  Delete elements from a dictionary
+
+myDict = {'name': 'Emmanuel', 'age': 20, 'state': 'Lagos', 'ethnicity': 'Igbo'}
+# print(myDict.pop('state'))
+# myDict.pop('state')
+print(myDict.popitem())
+print(myDict)

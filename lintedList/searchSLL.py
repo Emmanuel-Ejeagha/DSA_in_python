df0a0c71a3d6345ca64e67fee46d1ac23f25c21a
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-10-04 20:46:50 +0100

    Added: search singly linked list

---
+++ lintedList/searchSLL.py
@@ -0,0 +1,73 @@
+# Searching a sigly linked list
+
+# A singly linked list that contains two nodes
+
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
+
+class SLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next
+    #  Insert in Linked List
+    def insertSLL(self, value, location):
+        newNode = Node(value)
+        if self.head is None:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            if location == 0:
+                newNode.next = self.head
+                self.head = newNode
+            elif location == 1:
+                newNode.next =  None
+                self.tail.next = newNode
+                self.tail = newNode
+            else:
+                tempNode = self.head
+                index = 0 
+                while index < location - 1:
+                    tempNode = tempNode.next
+                    index += 1
+                nextNode = tempNode.next
+                tempNode.next = newNode
+                newNode.next = nextNode
+    
+    # Tranverse singly linked list
+    def traverseSLL(self):
+        if self.head is None:
+            print("The Singly Linked List does not exist")
+        else:
+            node = self.head
+            while node is not None:
+                print(node.value)
+                node = node.next
+
+    #  search for a node in a singly linked list
+    def searchSLL(self, nodeValue):
+        if self.head is None:
+            return "The list does not exist"
+        else:
+            node = self.head
+            while node is not None:
+                if node.value == nodeValue:
+                    return node.value
+                node = node.next
+            return "The value does not exist in the list"         
+
+singlyLinkedList = SLinkedList()
+singlyLinkedList.insertSLL(1, 1)
+singlyLinkedList.insertSLL(2, 1)
+singlyLinkedList.insertSLL(3, 1)
+singlyLinkedList.insertSLL(4, 1)
+singlyLinkedList.insertSLL(5, 1)
+print([node.value for node in singlyLinkedList])
+print(singlyLinkedList.searchSLL(5))
